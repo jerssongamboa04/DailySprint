49728c45a31667555087b5799deca60280479b2c
jerssongamboa04 <jerssongamboa350@gmail.com>
2023-04-20 20:32:35 +0200

    agrego funcionalidad a los botones

--- progresoporpasos/main.js
+++ progresoporpasos/main.js
@@ -1,22 +1,37 @@
+const btnNext = document.querySelectorAll('.btn');
+const divProgres = document.querySelector('.progress-container');
 
-const btns = document.querySelectorAll('.btn')
+let lastActiveIndex = 1;
 
-btns.forEach( (btn) => {
+btnNext.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    if (e.target.id === 'next') {
+      if (lastActiveIndex < divProgres.children.length) {
+        divProgres.children[lastActiveIndex].classList.add('active');
+        lastActiveIndex++;
+      }
+    } else if (e.target.id === 'prev') {
+      if (lastActiveIndex > 0) {
+        lastActiveIndex--;
+        divProgres.children[lastActiveIndex].classList.remove('active');
+      }
+    }
+    if (lastActiveIndex == divProgres.children.length) {
+      btnNext[1].disabled = true;
+      btnNext[0].disabled = false;
+     
+    }
+    if (lastActiveIndex < 4) {
+        btnNext[1].disabled = false;
+      }
+    if (lastActiveIndex == 1) {
+      btnNext[0].disabled = true;
+      btnNext[1].disabled = false;
+    }
 
-btn.addEventListener('click', () => { 
-
-    removeActiveClasses();
-    btn.classList.add('active');
-
-    console.log(btn);
-})
-});
-
-function removeActiveClasses() {
+    if (lastActiveIndex > 1) {
+        btnNext[0].disabled = false;
+      }
     
-    btns.forEach(btn => {
-        btn.classList.remove('active');
-    })
-
-
-}
+  });
+});
