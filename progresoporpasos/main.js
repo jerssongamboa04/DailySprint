f23ea0830fee2d5c8b325801b00ecd42cf9f4cba
jerssongamboa04 <jerssongamboa350@gmail.com>
2023-04-22 15:48:14 +0200

    funcionalidad progresoPorPasos

--- progresoporpasos/main.js
+++ progresoporpasos/main.js
@@ -1,37 +1,40 @@
 const btnNext = document.querySelectorAll('.btn');
 const divProgres = document.querySelector('.progress-container');
 
-let lastActiveIndex = 1;
+let lastActiveIndex = 0;
+const progressBarWidth = 33.33;
+
+console.log(divProgres.children.length);
 
 btnNext.forEach(btn => {
   btn.addEventListener('click', (e) => {
     if (e.target.id === 'next') {
       if (lastActiveIndex < divProgres.children.length) {
-        divProgres.children[lastActiveIndex].classList.add('active');
         lastActiveIndex++;
+        console.log(lastActiveIndex);
+        divProgres.children[lastActiveIndex + 1].classList.add('active');
+        divProgres.children[0].style.width = `${progressBarWidth * lastActiveIndex}%`;
       }
     } else if (e.target.id === 'prev') {
       if (lastActiveIndex > 0) {
+        divProgres.children[lastActiveIndex + 1].classList.remove('active');
         lastActiveIndex--;
-        divProgres.children[lastActiveIndex].classList.remove('active');
+        divProgres.children[0].style.width = `${progressBarWidth * lastActiveIndex}%`;
       }
     }
-    if (lastActiveIndex == divProgres.children.length) {
+    if (lastActiveIndex === divProgres.children.length - 2) {
       btnNext[1].disabled = true;
       btnNext[0].disabled = false;
-     
     }
-    if (lastActiveIndex < 4) {
-        btnNext[1].disabled = false;
-      }
-    if (lastActiveIndex == 1) {
+    if (lastActiveIndex < divProgres.children.length - 2) {
+      btnNext[1].disabled = false;
+    }
+    if (lastActiveIndex === 0) {
       btnNext[0].disabled = true;
       btnNext[1].disabled = false;
     }
-
-    if (lastActiveIndex > 1) {
-        btnNext[0].disabled = false;
-      }
-    
+    if (lastActiveIndex > 0) {
+      btnNext[0].disabled = false;
+    }
   });
 });
--- tarjetasExpansibles/main.js
+++ tarjetasExpansibles/main.js
@@ -1,24 +1,34 @@
+const btnNext = document.querySelectorAll('.btn');
+const divProgres = document.querySelector('.progress-container');
 
+let lastActiveIndex = 0;
 
-const panels = document.querySelectorAll('.panel')
-
-panels.forEach( (panel) => {
-
-panel.addEventListener('click', () => {
-
-removeActiveClasses();
-panel.classList.add('active');
-
-})
-
+btnNext.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    if (e.target.id === 'next') {
+      if (lastActiveIndex < divProgres.children.length - 1) {
+        divProgres.children[lastActiveIndex].classList.add('active');
+        divProgres.classList.add('active');
+        lastActiveIndex++;
+      }
+      if (lastActiveIndex === divProgres.children.length - 1) {
+        btnNext[1].classList.add('btn:disabled');
+      }
+      if (lastActiveIndex > 0) {
+        btnNext[0].classList.remove('btn:disabled');
+      }
+    } else if (e.target.id === 'prev') {
+      if (lastActiveIndex > 0) {
+        divProgres.children[lastActiveIndex - 1].classList.remove('active');
+        divProgres.classList.remove('active');
+        lastActiveIndex--;
+      }
+      if (lastActiveIndex === 0) {
+        btnNext[0].classList.add('btn:disabled');
+      }
+      if (lastActiveIndex < divProgres.children.length - 1) {
+        btnNext[1].classList.remove('btn:disabled');
+      }
+    }
+  });
 });
-
-function removeActiveClasses() {
-    
-    panels.forEach(panel => {
-
-        panel.classList.remove('active');
-    })
-
-
-}
